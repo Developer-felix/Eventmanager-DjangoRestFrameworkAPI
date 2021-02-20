380be96a4e5f258365306594e60d2eb640414d26
Developer-felix <onjombafelix1@gmail.com>
2021-02-19 21:15:17 -0800

    adding the heroku files

--- eventmanager/settings.py
+++ eventmanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
